a8a706805f12ca47a80ec447d4ca154874c9a2a7
Dimitry Doche <dimitry.doche@student.isae-supaero.fr>
2024-02-08 09:58:33 +0100

    M4

--- M4/Rust/src/bin/reversible_fact_fib.rs
+++ M4/Rust/src/bin/reversible_fact_fib.rs
@@ -1,5 +1,5 @@
 fn fact(n: u64) -> u64 {
-    let mut fact_n = 0;
+    let mut fact_n = 1;
     for i in 2..=n {
         fact_n *= i;
     }
@@ -20,6 +20,35 @@ fn fib_rec(n: u64) -> u64 {
     } else {
         fib_rec(n - 2) + fib_rec(n - 1)
     }
+}
+
+
+pub mod exo1 {
+    #[derive(Debug)]
+    enum Result {
+        Ok, 
+        Undef,
+        ArithError,
+    }
+    pub fn fact(n : u64, fact_n : &mut u64) -> Result {
+        *fact_n = 1;
+        for i in 2..n {
+            if let Some(res) = u64::checked_mul(*fact_n, i){
+                *fact_n = res;
+            }
+            else {
+                return Result::ArithError;
+            }
+        }
+        return Result::Ok
+    }
+    pub fn test_fact(){
+        let mut fact_n:u64 = 0;
+        let res = fact(5, &mut fact_n);
+        println!("fact(5) = {}, return {:?}", fact_n, res);
+    }
+
+    
 }
 
 fn main() {
---
+++ M4/Rust/target/debug/deps/reversible_fact_fib-7ee6b82df22c8529.d
@@ -0,0 +1,5 @@
+/home/dimitry/tcs1-in-labs/M4/Rust/target/debug/deps/libreversible_fact_fib-7ee6b82df22c8529.rmeta: src/bin/reversible_fact_fib.rs
+
+/home/dimitry/tcs1-in-labs/M4/Rust/target/debug/deps/reversible_fact_fib-7ee6b82df22c8529.d: src/bin/reversible_fact_fib.rs
+
+src/bin/reversible_fact_fib.rs:
