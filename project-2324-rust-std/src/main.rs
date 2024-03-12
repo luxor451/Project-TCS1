729934144c2d7425681d846007cd82544dbe544a
Dimitry Doche <dimitry.doche@student.isae-supaero.fr>
2024-03-12 16:16:24 +0100

    [project], change in main to only display the computed maze, need to update the tests

--- project-2324-rust-std/src/main.rs
+++ project-2324-rust-std/src/main.rs
@@ -1,4 +1,4 @@
-//! This project since it is in Rust does not respect completly the struture given in the subject
+//! This project, since it is in Rust, does not respect completly the struture given in the subject
 //! To use prim's algorithme, you should use the main i.e. run "cargo run --bin projet -- filename"
 //! This will display in the terminal the maze generated with prim_naive
 //!
@@ -25,19 +25,21 @@ fn main() {
     let filename: &String = &args[1];
     
     //Matrix test
-    println!("\nMatrix test :\n");
+    /* println!("\nMatrix test :\n");
     let mut mv: Matrix<i32> = Matrix::new(vec![1, 2, 3, 4, 5, 6], 2, 3);
     *mv.get_mut(1, 2) = 10;
     println!("Display:\n {}", mv);
-    println!("Debug: \n{:?}", mv);
+    println!("Debug: \n{:?}", mv); */
     //load_map test
-    println!("\nload_map test :\n");
+    /* println!("\nload_map test :\n"); */
     let test_map: Map = load_map(filename.to_string());
-    println!("Display:\n {}", test_map.edges_matrix);
+    /* println!("Display:\n {}", test_map.edges_matrix); */
     let prim: (Vec<i32>, i32) = prim_naive_function(&test_map);
+    // Get the dimentions from the filename
+    let numbers: Vec<&str>  = filename.split('_').collect();
     let maze_2048: Maze = Maze {
-        width: 10,
-        height: 8,
+        width: numbers[1].parse::<usize>().ok().unwrap(),
+        height: numbers[2].parse::<usize>().ok().unwrap(),
         predecessor: prim.0,
         cost: prim.1,
     };
